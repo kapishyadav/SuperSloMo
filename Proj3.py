221e24d91f35ae1e61de6434c8a0144b3dc430fa
Nick Kroeger <kroegern1@live.com>
2020-12-03 17:28:56 -0500

    added some equations and got output from 2nd net

--- Proj3.py
+++ Proj3.py
@@ -16,20 +16,17 @@ from torchvision.transforms import ToTensor
 random.seed(0)
 
 def my_test_train_split(filenames,train_perc):
-    random.shuffle(filenames)
-    train_len = int(train_perc*0.01*len(filenames))
-    train_names = filenames[:train_len]
-    test_names = filenames[train_len:]
-    return train_len, train_names, test_names
+	random.shuffle(filenames)
+	train_len = int(train_perc*0.01*len(filenames))
+	train_names = filenames[:train_len]
+	test_names = filenames[train_len:]
+	return train_len, train_names, test_names
 
 
+# data_path = "ExtractedImages/"
+# filenames = os.listdir(data_path)
 
-
-
-data_path = "ExtractedImages/"
-filenames = os.listdir(data_path)
-
-train_len, train_files, test_files = my_test_train_split(filenames, 80)
+# train_len, train_files, test_files = my_test_train_split(filenames, 80)
 
 
 # def save_train(train_files, test_files, train_len):
@@ -49,21 +46,6 @@ train_len, train_files, test_files = my_test_train_split(filenames, 80)
 # 	np.save("train.npy", train)
 # 	print("\n\nTrain data saved\n")
 # save_train(train_files, test_files, train_len)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def resize_tensor(input_tensors, h, w):
@@ -99,7 +81,7 @@ def backwarp(image, flow):
 	flow = flow.squeeze()
 	flowX = flow[0,:,:]
 	flowY = flow[1,:,:]
-	warpedImg = cv2.remap(image, flowX, flowY, cv2.INTER_LINEAR)
+	warpedImg = cv2.remap(image.squeeze().transpose(1,2,0), flowX, flowY, cv2.INTER_LINEAR)
 	return warpedImg
 
 
@@ -124,17 +106,16 @@ if torch.cuda.is_available():
 	flowCompNet   = flowCompNet.cuda()
 	arbFlowInterp = arbFlowInterp.cuda()
 
-
 ##############################
 # Define Training Algorithm
 ##############################
 train_loss = []
+NumIntermediateFrames = 6
 epochs     = 100
 for i in range(0, epochs):
 	print("epoch: ", i+1)
 
 	#TODO: Get 2 frames and intermediate frames
-
 	#Define input to 1st network
 	ImageInput = torch.cat([img0, img1], dim=0).permute(1,0,2,3)
 	# ImageInput = torch.unsqueeze(ImageInput, dim = 0)
@@ -144,32 +125,48 @@ for i in range(0, epochs):
 	Flow1to0   = Flows[:,2:,:,:]
 
 	#Define input to 2nd network
-
-	for i in range(1,7):
-		t=i/7
+	rLoss, pLoss = 0.0, 0.0
+	for i in range(1,NumIntermediateFrames+1):
+		t=i/(NumIntermediateFrames+1)
+		# I_true_t =  Images[t] #groundtruth intermediate frame
 		#Calculate backwarp g() twice for t->I1 and t->I0 (3 channels for each)
 		img0_np     = img0.detach().numpy()
 		Flow0to1_np = Flow0to1.detach().numpy()
-		warpedImg   = backwarp(img0_np, Flow0to1_np)
-		import pdb; pdb.set_trace()
-		#Calculate F_hat from equation (4)
+		img1_np     = img1.detach().numpy()
+		Flow1to0_np = Flow1to0.detach().numpy()
 
-		F_hat_tto0 = -(1-t)*t*Flow0to1 + t*t*Flow1to0
-		F_hat_tto1 = (1-t)*(1-t)* Flow0to1 + t*(1-t)*Flow1to0
+		#Calculate F_hat from equation (4)
+		F_hat_tto0 = -(1-t)*t*Flow0to1_np + t*t*Flow1to0_np
+		F_hat_tto1 = (1-t)*(1-t)* Flow0to1_np - t*(1-t)*Flow1to0_np
+
+		g0 = torch.from_numpy(backwarp(img0_np, F_hat_tto0)).permute(2,0,1).unsqueeze(0)
+		g1 = torch.from_numpy(backwarp(img1_np, F_hat_tto1)).permute(2,0,1).unsqueeze(0)
+
+		# Input to network 2 - arb time flow interpolation
+		# 20 = 6 RGB channels + 4 Flows + 6 backwarped images + 4 Flows from prev net
+		arbFlowInput = torch.cat([ImageInput, g1, torch.from_numpy(F_hat_tto1), torch.from_numpy(F_hat_tto0), g0, Flow0to1, Flow1to0], dim=1)
+
 		#forward pass through 2nd network
-		arbFlowOutputs = arbFlowInterp()
-		V_t1      = arbFlowOutputs[:,  0,:,:]
+		arbFlowOutputs = arbFlowInterp(arbFlowInput)
+
+		V_t1    = arbFlowOutputs[:,  0,:,:]
 		DeltaF_t1 = arbFlowOutputs[:,1:3,:,:]
 		DeltaF_t0 = arbFlowOutputs[:,3: ,:,:]
+		V_t0	  = 1 - V_t1 #equation (5)
 
 		#Calculate predicted I_t using equation (1.5) from the paper
-		# I_tHat = ~
+		Z = (1-t)*V_t0 + t*V_t1
+		I_tHat = ((1-t)*V_t0*g0 + t*V_t1*g1)/Z
+		import pdb; pdb.set_trace()
 
-		#Calculate the losses with weights equation (7)
-		predError = rLoss + pLoss + wLoss + sLoss
+		rLoss += np.linalg.norm((I_tHat - I_true_t), ord=1)
+		pLoss += perceptual_loss(I_tHat, I_t)
+	#Calculate the losses with weights equation (7)
+	predError = rLoss/NumIntermediateFrames + pLoss/NumIntermediateFrames + wLoss + sLoss
 
-		#Update the weights
-		predError.backward()
-		optimizer.step()
+	#Update the weights
+	predError.backward()
+	optimizer.step()
+
 
 #TODO: Given a 30fps video, predict intermediate frames and save out a 240fps video
